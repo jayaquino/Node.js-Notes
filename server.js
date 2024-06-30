56ff1f7bd89d56ef96ea9f941dad7ab456773f1a
Nelson Aquino Jr. <naquinojr@outlook.com>
2024-06-30 09:39:53 -0700

    [Feature] Create tour schema

--- server.js
+++ server.js
@@ -23,6 +23,33 @@ const app = require('./app');
 
 console.log(process.env);
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [
+      true,
+      'A tour must have a name',
+    ],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [
+      true,
+      'A tour must have a price',
+    ],
+  },
+});
+
+const Tour = mongoose.model(
+  'Tour',
+  tourSchema,
+);
+
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(
