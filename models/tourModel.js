dec609c10a2a7baa6fa4f283846be142ec793388
Nelson Aquino Jr. <naquinojr@outlook.com>
2024-07-04 18:06:55 -0700

    [Feature] Update Tour model to be more complicated

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,10 +8,36 @@ const tourSchema = new mongoose.Schema({
       'A tour must have a name',
     ],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [
+      true,
+      'A tour must have a duration',
+    ],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [
+      true,
+      'A tour must have a group size',
+    ],
+  },
+  difficulty: {
+    type: String,
+    required: [
+      true,
+      'A tour must have a difficulty',
+    ],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
@@ -20,6 +46,32 @@ const tourSchema = new mongoose.Schema({
       'A tour must have a price',
     ],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true, // Removes white space in beginning and end
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: [
+      true,
+      'A tour must have a description',
+    ],
+  },
+  imageCover: {
+    type: String,
+    required: [
+      true,
+      'A tour must have a cover image',
+    ],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model(
