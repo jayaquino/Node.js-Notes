90bf40013896b2b2367efee48b5dbf7cc4bbb0d8
Nelson Aquino Jr. <naquinojr@outlook.com>
2024-07-04 18:51:52 -0700

    [Feature] Create script to load local data

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,66 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const Tour = require('./../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((connection) => {
+    console.log(
+      'DB connection successful',
+    );
+  });
+
+// READ JSON FILE
+const tours = JSON.parse(
+  fs.readFileSync(
+    `${__dirname}/tours-simple.json`,
+    'utf-8',
+  ),
+);
+
+// IMPORT DATA TO DATABASE
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log(
+      'DATA successfully loaded',
+    );
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// DELETE ALL DATA FROM COLLECTION
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany(); // Delete all documents in Tours
+    console.log(
+      'DATA successfully deleted',
+    );
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+console.log(process.argv);
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (
+  process.argv[2] === '--delete'
+) {
+  deleteData();
+}
