bf33bbfab7c46420f005807405a8a6018b7e7fe5
Nelson Aquino Jr. <naquinojr@outlook.com>
2024-07-06 23:53:05 -0700

    [Feature] Query middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -74,6 +74,10 @@ const tourSchema = new mongoose.Schema(
       default: Date.now(),
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -107,6 +111,36 @@ tourSchema.pre('save', function (next) {
 //     next();
 //   },
 // );
+
+// QUERY MIDDLEWARE
+tourSchema.pre(
+  /^find/, // Regex to query everything that starts with find
+  function (next) {
+    this.find({
+      secretTour: { $ne: true },
+    });
+    this.start = Date.now();
+    next();
+  },
+);
+
+tourSchema.post(
+  /^find/,
+  function (docs, next) {
+    console.log(
+      `Query took ${Date.now() - this.start} milliseconds`,
+    );
+    console.log(docs);
+    next();
+  },
+);
+
+// tourSchema.pre('find' or 'findOne', function (next) {
+//   this.find({
+//     secretTour: { $ne: true },
+//   });
+//   next();
+// });
 
 const Tour = mongoose.model(
   'Tour',
