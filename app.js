6188984787de67912bc105b151654a605b817c8d
Nelson Aquino Jr. <naquinojr@outlook.com>
2024-10-14 20:52:34 -0700

    [Feature] Global error handling

--- app.js
+++ app.js
@@ -47,9 +47,26 @@ app.use('/api/v1/users', userRouter);
 
 // If all other routes are not matched, it will his this route.
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl}`
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl}`
+  // });
+  const err = new Error(
+    `Can't find${req.originalUrl} on this server!`
+  );
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+});
+
+// Using four parameters let's express know this is an error middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   });
 });
 
