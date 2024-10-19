1f2d62dd5921f17bd86bcaf3c246a4b9127a2318
Nelson Aquino Jr. <naquinojr@outlook.com>
2024-10-19 11:56:07 -0700

    [Feature] Unhandled rejections

--- server.js
+++ server.js
@@ -22,8 +22,15 @@ const app = require('./app');
 console.log(process.env);
 
 const port = process.env.PORT;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
 
 // TEST
