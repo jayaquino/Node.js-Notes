6dd9e4a17d5bd4921be44cf59bbc7e9b6e41d314
Nelson Aquino Jr. <naquinojr@outlook.com>
2024-10-20 10:22:23 -0700

    [Feature] Prevent user from registering a specific role

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,14 @@ const User = require('../models/userModel');
 const catchAsync = require('../utilities/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  // Only allow data that we need, user cannot manually input a role
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm
+  });
+
   res.status(201).json({
     status: 'success',
     data: {
