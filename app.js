0175ee4a2a8c11694c58ee92a97b65c17e5727fe
Nelson Aquino Jr. <naquinojr@outlook.com>
2024-10-26 22:42:40 -0700

    [Feature] Rate limiting using express-rate-limit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utilities/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -13,6 +14,17 @@ console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Allows 100 request in 1 hr
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message:
+    'Too many requests form this IP, please try again in an hour'
+});
+
+app.use('/api', limiter);
+
 app.use(express.json());
 
 app.use((request, response, next) => {
