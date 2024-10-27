4b82df8f696a5f387d75aabb0e1d3313dea34bed
Nelson Aquino Jr. <naquinojr@outlook.com>
2024-10-26 23:00:46 -0700

    [Feature] Prevent parameter pollution with hpp

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utilities/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -39,6 +40,20 @@ app.use(mongoSanitize());
 
 // Data sanitization against XSS, clean input from html code. Injected html code with js code.
 app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
 
 // Test middleware
 app.use((request, response, next) => {
