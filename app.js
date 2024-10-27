5563e338e2ae408f87515d3eb6aefb052becb38b
Nelson Aquino Jr. <naquinojr@outlook.com>
2024-10-26 22:55:35 -0700

    [Feature] Data sanitization with express-mongo-santize and xss-clean

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utilities/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -31,6 +33,12 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS, clean input from html code. Injected html code with js code.
+app.use(xss());
 
 // Test middleware
 app.use((request, response, next) => {
