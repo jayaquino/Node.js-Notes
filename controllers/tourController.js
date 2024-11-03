d03634094853fe8182c640b13f22731b5986d320
Nelson Aquino Jr. <naquinojr@outlook.com>
2024-11-02 18:03:24 -0700

    [Fix] Delete tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -94,7 +94,9 @@ exports.deleteTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findByIdAndDelete(req.params.id);
 
   if (!tour) {
-    next(new AppError('No tour found with that ID', 404));
+    return next(
+      new AppError('No tour found with that ID', 404)
+    );
   }
 
   res.status(204).json({
