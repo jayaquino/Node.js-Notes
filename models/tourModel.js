0c4b54782982bb69c1510b92f8c3269abe06ebe0
Nelson Aquino Jr. <naquinojr@outlook.com>
2024-11-10 12:20:10 -0800

    feature(geospatial): aggregate geoNear

--- models/tourModel.js
+++ models/tourModel.js
@@ -127,6 +127,7 @@ const tourSchema = new mongoose.Schema(
 tourSchema.index({ price: 1, ratingsAverage: -1 });
 tourSchema.index({ slug: 1 });
 tourSchema.index({ startLocation: '2dsphere' });
+
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
@@ -210,16 +211,16 @@ tourSchema.pre(/^find/, function (next) {
 // });
 
 // AGGREGATION MIDDLEWARE
-tourSchema.pre('aggregate', function (next) {
-  this.pipeline().unshift({
-    // Unshift adds beginning of array, shift adds end of the array
-    $match: {
-      secretTour: { $ne: true }
-    }
-  });
-  console.log(this.pipeline());
-  next();
-});
+// tourSchema.pre('aggregate', function (next) {
+//   this.pipeline().unshift({
+//     // Unshift adds beginning of array, shift adds end of the array
+//     $match: {
+//       secretTour: { $ne: true }
+//     }
+//   });
+//   console.log(this.pipeline());
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -27,6 +27,10 @@ router
   .get(tourController.getToursWithin);
 
 router
+  .route('/distances/:latlng/unit/:unit')
+  .get(tourController.getDistances);
+
+router
   .route('/tour-stats')
   .get(tourController.getTourStats);
 
