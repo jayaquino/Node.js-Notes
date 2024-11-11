588fbd94c4d8f7b72f1685727cefe024561a89b6
Nelson Aquino Jr. <naquinojr@outlook.com>
2024-11-10 17:26:28 -0800

    feature(pug): first steps with pug - styles, interpolation, js, const

--- app.js
+++ app.js
@@ -91,7 +91,10 @@ app.post('/', (request, response) => {
 // app.delete('/api/v1/tours/:id', deleteTour);
 
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Jonas'
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
