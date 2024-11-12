8334cd0d7d6faf19327f6f7695f260420b35fb75
Nelson Aquino Jr. <naquinojr@outlook.com>
2024-11-11 16:33:00 -0800

    feature(pug): update user data using API

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,16 @@
 /* eslint-disable */
 import '@babel/polyfill';
 import { login, logout } from './login';
+import { updateData } from './updateSettings';
 
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector(
   '.nav__el--logout'
 );
+const userDataForm = document.querySelector(
+  '.form-user-data'
+);
+
 if (loginForm)
   document
     .querySelector('.form')
@@ -18,3 +23,11 @@ if (loginForm)
     });
 
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
+
+if (userDataForm)
+  userDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateData(name, email);
+  });
