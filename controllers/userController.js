250c7e61f1a6bd7a096e01fb8cc8222f6db9e5ab
Nelson Aquino Jr. <naquinojr@outlook.com>
2024-11-11 17:35:14 -0800

    feature(multer): change DB photo name to file name

--- controllers/userController.js
+++ controllers/userController.js
@@ -51,8 +51,6 @@ exports.getMe = (req, res, next) => {
 };
 
 exports.updateMe = catchAsync(async (req, res, next) => {
-  console.log(req.file);
-  console.log(req.body);
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
@@ -65,6 +63,9 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
   // 2) Update user document
   const filteredBody = filterObj(req.body, 'name', 'email');
+
+  if (req.file) filteredBody.photo = req.file.filename;
+
   const updatedUser = await User.findByIdAndUpdate(
     req.user.id,
     filteredBody,
